f90614255eb1c8677e461ad9a7c789532dc4bce7
Jeongho Jeong <jeongho.jeong12@gmail.com>
2021-06-03 19:59:57 +0000

    Save step

--- src/public/fb.js
+++ src/public/fb.js
@@ -1,5 +1,33 @@
-window.fbAsyncInit = () => {
-  /* TODO: implement */
-}
-
 // TODO: initialize Facebook SDK
+window.fbAsyncInit = () => {
+  FB.init({
+    appId: APP_CONFIG.FB_APP_ID,
+    version: 'v10.0',
+  })
+
+  document.getElementById('fb-login').addEventListener('click', () => {
+    FB.login(
+      (response) => {
+        // handle the response
+        console.log(response)
+        fetch(
+          `/users/auth/facebook?access_token=${response.authResponse.accessToken}`,
+          {
+            method: 'POST',
+          }
+        )
+      },
+      { scope: 'public_profile,email' }
+    )
+  })
+}
+;((d, s, id) => {
+  const fjs = d.getElementsByTagName(s)[0]
+  if (d.getElementById(id)) {
+    return
+  }
+  const js = d.createElement(s)
+  js.id = id
+  js.src = 'https://connect.facebook.net/en_US/sdk.js'
+  fjs.parentNode.insertBefore(js, fjs)
+})(document, 'script', 'facebook-jssdk')
